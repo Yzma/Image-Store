bd5929bc8cccc3d5e73211455a5e7dfe3f61103f
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-05 13:51:23 -0400

    Added joi schemsa file to handle image info validation

---
+++ util/joiSchemas.js
@@ -0,0 +1,24 @@
+
+import Joi from "joi";
+
+const CLEAN_NAME_REGEX = /^[a-zA-Z0-9\-]+$/
+
+const VALID_LABEL_NAME = Joi.string()
+                .pattern(CLEAN_NAME_REGEX, { name: 'Valid Image Name Regex' })
+                .min(1)
+                .max(30)
+
+export const imageSchema = Joi.object({
+    title: VALID_LABEL_NAME
+        .alter({
+            create: (schema) => schema.required(),
+            update: (schema) => schema.optional()
+        }),
+
+    description: Joi.string()
+        .max(150)
+        .alter({
+            create: (schema) => schema.optional(),
+            update: (schema) => schema.optional(),
+        }),
+});
