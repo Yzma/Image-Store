d05a2b2aa89c45a9ffae53732ff47d5fc4cdbca2
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-05 11:31:54 -0400

    Added constant variables for uploading images

---
+++ util/constants.js
@@ -0,0 +1,9 @@
+
+// The folder to store all uploaded images
+export const IMAGE_DESTINATION_FOLDER = './uploaded-images/';
+
+// An array of accepted file mime types
+export const ACCEPTED_FILE_UPLOAD_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png"]
+
+// The amount of images that can be uploaded at a time
+export const MAX_IMAGES_PER_UPLOAD = 15
