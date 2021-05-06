06bde352a00502509fc7aafe751e0f3078809601
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-06 09:42:07 -0400

    Implement getUser in userUtil to return user info

--- util/database/userUtil.js
+++ util/database/userUtil.js
@@ -1,5 +1,20 @@
 import prisma from "../prisma"
 
+export function getUser(userID) {
+    return prisma.user.findFirst({
+        where: {
+            id: parseInt(userID)
+        },
+
+        select: {
+            name: true,
+            image: true,
+            createdAt: true,
+            money: true
+        }
+    })
+}
+
 export const getUserFromAccessToken = async (accessToken) => {
     
     if(!accessToken)
