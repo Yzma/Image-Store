d8edc45d65903caf373041a1b3f401cbc22fc857
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-07 00:31:38 -0400

    Implemented changebalance route to allow altering the users balance

---
+++ pages/api/v1/users/[userID]/changebalance.js
@@ -0,0 +1,36 @@
+import prisma from "../../../../../util/prisma"
+
+import { userBalanceSchema } from '../../../../../util/joiSchemas'
+
+export default async (req, res) => {
+
+    const { userID } = req.query
+
+    /*
+        Updates the users balance. This route is intentionally left to be open to edit the users money.
+    */
+    if (req.method === "PATCH") {
+        
+        const { balance } = req.body
+
+        const { error } = userBalanceSchema.validate({ balance: balance })
+        if(error) {
+            return res.status(400).json({error: "Invalid data"})
+        }
+
+        const updatedUserBalance = await prisma.user.update({
+            where: {
+                id: parseInt(userID)
+            },
+
+            data: {
+                balance: balance 
+            }
+        })
+
+        return res.status(200).json(updatedUserBalance)
+
+    } else {
+        res.status(404).json({ error: `${req.method} is not supported` })
+    }
+}
