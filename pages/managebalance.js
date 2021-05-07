cb4a739e5537d4826f74be51e0afc33f62bce18b
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-07 12:38:45 -0400

    Implemented alert popups when editing balance

--- pages/managebalance.js
+++ pages/managebalance.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { PageTemplate } from '../components/PageTemplate'
-import { Col, Row, Form, Container, Button } from '@themesberg/react-bootstrap';
+import { Col, Row, Form, Container, Button, Alert } from '@themesberg/react-bootstrap';
 
-const ManageBalance = () => {
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
-  const handleSubmit = async (event) => {
-    event.preventDefault()
+import prisma from '../util/prisma';
 
+const ManageBalance = (props) => {
 
-    
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
 
-  };
+  const balanceView = useRef(props.balance);
+  const formik = useFormik({
+    initialValues: { balance: 15 },
+
+    validationSchema: Yup.object({
+      balance: Yup.number()
+        .moreThan(0, "Balance must be over $0")
+        .lessThan(1000, "Balance cannot be greater than $1000")
+        .required('Required')
+    }),
+
+    onSubmit: async (values) =>
+      fetch('http://localhost:3000/api/v1/users/1/changebalance', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ balance: values.balance }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+          balanceView.current = parseInt(data.balance)
+          setShowSuccessAlert(true)
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          setShowErrorAlert(true)
+        })
+  })
 
   return (
     <PageTemplate>
@@ -27,15 +58,29 @@ const ManageBalance = () => {
 
           <div className="account-border d-flex justify-content-between mb-5" />
 
+          <Alert show={showSuccessAlert} variant="success" onClose={() => setShowSuccessAlert(false)} dismissible>
+            <Alert.Heading>Success!</Alert.Heading>
+            <p>
+              Your balance has been updated to {balanceView.current.toFixed(2)}
+            </p>
+          </Alert>
+
+          <Alert show={showErrorAlert} variant="danger" onClose={() => setShowErrorAlert(false)} dismissible>
+            <Alert.Heading>Error!</Alert.Heading>
+            <p>
+              Looks like an error occurred on the backend. Please try again. If the error persists, please open a ticket on GitHub.
+            </p>
+          </Alert>
+
           <h5>Your Current Balance</h5>
           <p className="mb-0">
-            $0.00
+            ${balanceView.current.toFixed(2)}
           </p>
 
-          <Form className="py-3" onSubmit={handleSubmit}>
+          <Form onSubmit={formik.handleSubmit} className="py-3">
             <Form.Group className="mb-3">
               <Form.Label>New Balance</Form.Label>
-              <Form.Control type="text" className="form-control-sm" isInvalid />
+              <Form.Control name="balance" type="text" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.balance} className="form-control-sm" isInvalid={formik.errors.balance}/>
               <Form.Control.Feedback type="invalid">
                 Please enter a number between 0-1000.
               </Form.Control.Feedback>
@@ -47,12 +92,33 @@ const ManageBalance = () => {
             <Form.Group className="py-3">
                 <Button type="submit">Update Balance</Button>
             </Form.Group>
-
           </Form>
         </Container>
       </article>
     </PageTemplate>
   );
 };
+
+export async function getServerSideProps(context) {
+
+  const userBalance = await prisma.user.findFirst({
+    where: {
+      id: 1
+    },
+
+    select: {
+      balance: true
+    }
+  })
+
+  console.log(userBalance)
+
+  return {
+    props: {
+      balance: 150
+    }
+  }
+}
+
 
 export default ManageBalance
