2e8cf683fd78e706f1db06e1f37b6286542411a0
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-07 16:25:11 -0400

    Added MAX_IMAGES_PER_PAGE, changed IMAGE_DESTINATION_FOLDER and added ErrorCode enum

--- util/constants.js
+++ util/constants.js
@@ -1,6 +1,9 @@
 
+// The maximum number of images to be displayed on a single page (used for database Pagination)
+export const MAX_IMAGES_PER_PAGE = 12
+
 // The folder to store all uploaded images
-export const IMAGE_DESTINATION_FOLDER = './uploaded-images/';
+export const IMAGE_DESTINATION_FOLDER = './uploaded-images';
 
 // An array of accepted file mime types
 export const ACCEPTED_FILE_UPLOAD_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png"]
@@ -10,3 +13,8 @@ export const MAX_IMAGES_PER_UPLOAD = 15
 
 // The maximum size of a file in bytes
 export const MAX_FILE_SIZE_IN_BYTES = 1024 * 1024 // One mega-byte
+
+export const ErrorCode = {
+    NO_AUTHORIZATION: 0,
+    NOT_FOUND: 1
+}
