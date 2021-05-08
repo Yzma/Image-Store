7dcd958336cd71878502d21de97243328d9c22d0
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-08 12:20:13 -0400

    Use promises for user transactions route

--- pages/api/v1/users/[userID]/transactions/index.js
+++ pages/api/v1/users/[userID]/transactions/index.js
@@ -1,4 +1,8 @@
-import prisma from "../../../../../util/prisma"
+
+import { transactionModeSchema } from '../../../../../../util/joiSchemas'
+import { getUserTransactions } from '../../../../../../util/database/userUtil'
+
+import { ValidationError } from 'joi'
 
 export default async (req, res) => {
 
@@ -6,8 +10,21 @@ export default async (req, res) => {
 
     if (req.method === "GET") {
         
-    
-        return res.status(404).json({ error: 'Not implemented' })
+        const { option } = req.body
+
+        const userTransactions = transactionModeSchema.validateAsync({ option: option })
+            .then(() => getUserTransactions(userID, option))
+            .then((data) => {
+                return res.status(200).json(data)
+            })
+            .catch((error) => {
+                if(error instanceof ValidationError) {
+                    return res.status(400).json({error: 'Invalid data'})
+                }
+                return res.status(400).json({error: 'Not found'})
+            })
+
+        return await userTransactions
 
     } else {
         res.status(404).json({ error: `${req.method} is not supported` })
