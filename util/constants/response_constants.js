5a4065a4db765e85f3fc2909fdff5572999c7964
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-08 16:08:40 -0400

    Added INVALID_SESSION_DATA, USER_NOT_FOUND and METHOD_NOT_SUPPORTED strings to response constants

--- util/constants/response_constants.js
+++ util/constants/response_constants.js
@@ -6,3 +6,9 @@ export const NO_AUTHORIZATION = "You are not authorized to view this resource"
 export const INVALID_DATA = "Invalid data"
 
 export const INTERNAL_SERVER_ERROR = "Internal Server Error"
+
+export const INVALID_SESSION_DATA = "Invalid session data"
+
+export const USER_NOT_FOUND = "User not found"
+
+export const METHOD_NOT_SUPPORTED = "Method is not supported on this route"
