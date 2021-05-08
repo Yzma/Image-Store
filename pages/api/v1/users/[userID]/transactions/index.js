bf7eac7998ee068eb1e9151c607124e0dc0220b9
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-08 16:11:35 -0400

    Cleanup and check user authentication for transactions route

--- pages/api/v1/users/[userID]/transactions/index.js
+++ pages/api/v1/users/[userID]/transactions/index.js
@@ -1,8 +1,10 @@
 
 import { transactionModeSchema } from '../../../../../../util/joiSchemas'
-import { getUserTransactions } from '../../../../../../util/database/userUtil'
+import { getUserTransactions, getAuthenticatedUserFromRequest } from '../../../../../../util/database/userUtil'
 
+import { INTERNAL_SERVER_ERROR, METHOD_NOT_SUPPORTED } from '../../../../../../util/constants/response_constants'
 import { ValidationError } from 'joi'
+import { InvalidUserError } from '../../../../../../util/errors'
 
 export default async (req, res) => {
 
@@ -13,6 +15,7 @@ export default async (req, res) => {
         const { option } = req.body
 
         return await transactionModeSchema.validateAsync({ option: option })
+            .then(() => getAuthenticatedUserFromRequest(req))
             .then(() => getUserTransactions(userID, option))
             .then((data) => {
                 return res.status(200).json(data)
@@ -20,11 +23,14 @@ export default async (req, res) => {
             .catch((error) => {
                 if(error instanceof ValidationError) {
                     return res.status(400).json({error: 'Invalid data'})
+                } else if(error instanceof InvalidUserError) {
+                    return res.status(400).json({error: error.errorDescription})
                 }
-                return res.status(400).json({error: 'Not found'})
+
+                return res.status(500).json({error: INTERNAL_SERVER_ERROR})
             })
 
     } else {
-        res.status(404).json({ error: `${req.method} is not supported` })
+        res.status(404).json({ error: METHOD_NOT_SUPPORTED })
     }
 }
