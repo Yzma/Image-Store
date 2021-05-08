3e9017d89e7916449bb0883c55073a5acba1cbf7
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-08 19:51:30 -0400

    Implement permissions when uploading files and correct error handling

--- pages/api/v1/users/[userID]/images/index.js
+++ pages/api/v1/users/[userID]/images/index.js
@@ -1,12 +1,14 @@
 
 import prisma from '../../../../../../util/prisma'
-import { getUserFromAccessToken, getUserByReq, getImages } from '../../../../../../util/database/userUtil'
-import { getSession } from 'next-auth/client'
 
-import * as ResponseConstants from '../../../../../../util/constants/response_constants'
+import { getAuthenticatedUserFromRequest } from '../../../../../../util/database/userUtil'
+import { getImages } from '../../../../../../util/database/imageRepository/localFileImageRepository'
+
+import { METHOD_NOT_SUPPORTED, NO_AUTHORIZATION, INTERNAL_SERVER_ERROR } from '../../../../../../util/constants/response_constants'
 
 import { MulterMiddleware, InvalidFileTypeError } from '../../../../../../util/middleware/multer'
-import multer from 'multer';
+import { InvalidUserError } from '../../../../../../util/errors'
+import { MulterError } from 'multer'
 
 export default async (req, res) => {
 
@@ -44,35 +46,21 @@ export default async (req, res) => {
     // POST request to upload image
     } else if (req.method === "POST") {
 
-        // TODO: Check permissions
-        const promise = new Promise(async (resolve, reject) => {
+        return await getAuthenticatedUserFromRequest(req)
+            .then((data) => {
 
-            const session = await getSession({ req })
-            if (!session) {
-                return reject(null)
-            }
+                // Check if the user is who it claims to be
+                if(data.id != userID) {
+                    throw new InvalidUserError(NO_AUTHORIZATION)
+                }
 
-            const user = await getUserByReq(session)
-            if (!user) {
-                return reject(null)
-            }
-
-            // Check if the user is who it claims to be
-            if (user.id != parseInt(userID)) {
-                return reject(null)
-            }
-
-            console.log(user)
-            return resolve(user)
-
-        })
-
-            .then(() => {
+                return data
+            })
+            .then(() =>
 
                 // After we confirm the user exists and authenticated, let's start uploading.
-                return MulterMiddleware(req, res)
-            })
-
+                MulterMiddleware(req, res)
+            )
             .then(() => {
 
                 // After multer has finished validating and processing the incoming files, we then create an array of transactions
@@ -110,15 +98,15 @@ export default async (req, res) => {
             ).then((value) => {
 
                 // After everything is complete, return that all files were successfully uploaded
-                console.log(value)
-                return res.status(200).json({ success: "Made it here!" })
+                return res.status(200).json({ success: value })
             })
 
             .catch((error) => {
 
                 // The error handling here is a little weird due to how Multer does it's error handling.
                 // This catches all the errors that are most likely to happen, and returns a 500 if it's a different error.
-                if (error instanceof multer.MulterError) {
+                if (error instanceof MulterError) {
+                    console.log(error)
                     if (error.code == 4) {
                         return res.status(400).json({ error: 'Files are too large' })
                     } else if (error.code == 5) {
@@ -126,6 +114,8 @@ export default async (req, res) => {
                     }
                 } else if (error instanceof InvalidFileTypeError && error.name === "InvalidFileTypeError") {
                     return res.status(400).json({ error: 'Invalid file types' })
+                } else if(error instanceof InvalidUserError) {
+                    return res.status(400).json({error: error.errorDescription})
                 }
 
                 // Unhandled - something else failed somewhere
@@ -133,10 +123,8 @@ export default async (req, res) => {
                 return res.status(500).json({ error: 'Error uploading files' })
             })
 
-        return await promise
-
     } else {
-        res.status(404).json({ error: `${req.method} is not supported` })
+        res.status(404).json({ error: METHOD_NOT_SUPPORTED })
     }
 }
 
