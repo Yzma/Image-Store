dfe3d677d89523d367db45e13d488602653df453
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-08 12:33:04 -0400

    Cleanup multiple image delete route

--- pages/api/v1/users/[userID]/images/delete.js
+++ pages/api/v1/users/[userID]/images/delete.js
@@ -2,48 +2,27 @@
 import prisma from '../../../../../../util/prisma'
 import { getSession } from 'next-auth/client'
 
-// import { deleteImages } from '../../../../../../util/database/imageRepository/localFileImageRepository'
+import { deleteImages } from '../../../../../../util/database/imageRepository/localFileImageRepository'
 import { imageDeleteSchema } from '../../../../../../util/joiSchemas'
+
+import { ValidationError } from 'joi'
 
 export default async (req, res) => {
 
     // DELETE image by ID - TODO: Figure out how to return API code for this
     if (req.method === "DELETE") {
 
-        const { ids } = req.body
+        const { userID, ids } = req.body
 
-        try {
-
-            const { error } = imageDeleteSchema.validate({ ids: ids })
-            if(error) {
-                return res.status(400).json({error: "Invalid data"})
-            }
-
-            // Just assume
-            const userID = 2
-
-            const deletedImages = await prisma.image.deleteMany({
-                where: {
-                    AND: [
-                        {
-                            id: {
-                                in: ids
-                            }
-                        },
-
-                        {
-                            userID: userID
-                        }
-                    ]
+        return await imageDeleteSchema.validateAsync({ ids: ids })
+            .then(() => deleteImages(userID, ids))
+            .then((data) => res.status(200).json(data))
+            .catch((error) => {
+                if(error instanceof ValidationError) {
+                    return res.status(400).json({error: 'Invalid data'})
                 }
+                return res.status(400).json({error: 'Not found'})
             })
-
-            return res.status(200).json(deletedImages)
-            
-        } catch(err) {
-            console.error(err)
-            return res.status(500).json({error: 'Internal Server Error'})
-        }
 
     } else {
         res.status(404).json({ error: `${req.method} is not supported` })
