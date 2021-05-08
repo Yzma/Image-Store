26fdbfde9eb1019e60a3c171937f57aa7cdfebe2
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-08 13:10:29 -0400

    Created responce_constants file to return common responses

---
+++ util/constants/response_constants.js
@@ -0,0 +1,8 @@
+
+export const NOT_FOUND = "Resource not found"
+
+export const NO_AUTHORIZATION = "You are not authorized to view this resource"
+
+export const INVALID_DATA = "Invalid data"
+
+export const INTERNAL_SERVER_ERROR = "Internal Server Error"
