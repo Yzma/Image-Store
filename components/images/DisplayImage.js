6d4226e08d0ada599dc467b9268becbc18fe3a25
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-09 00:28:16 -0400

    Use correct URL and added lock icon for private images for DisplayImage component

--- components/images/DisplayImage.js
+++ components/images/DisplayImage.js
@@ -1,16 +1,22 @@
 
 import { Col, Card, Image } from '@themesberg/react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 export const DisplayImage = (props) => {
 
-  const { title, description, imageURL } = props.image
+  const { id, title, private: isPrivate, description, imageURL, fileName } = props.image
 
+  const actualImageURL = `http://localhost:3000/api/v1/images/${fileName}`
   return (
     <Col>
       <Card className="h-100">
-        <a href="/"><Image src={imageURL} className="card-img-top" /></a>
+        <a href={actualImageURL}><Image src={actualImageURL} className="card-img-top" /></a>
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title>
+            {title} {isPrivate ? <FontAwesomeIcon icon={faLock}/> : null}
+          </Card.Title>
+          
           <Card.Text>{description}</Card.Text>
         </Card.Body>
       </Card>
