7cb4950b78e514dbe508dff12fceed927399bee6
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-09 15:25:23 -0400

    Cleanup ProfileCard component

--- components/profile/ProfileCard.js
+++ components/profile/ProfileCard.js
@@ -5,15 +5,16 @@ import { Card } from '@themesberg/react-bootstrap';
 import DefaultProfilePicture from "../../public/assets/img/team/profile-picture-1.jpg";
 
 export const ProfileCard = (props) => {
+
     const { name, balance, image } = props.userInformation;
-  
+
     return (
       <Card border="light" className="text-center p-0 mb-4">
         <div className="profile-cover rounded-top" />
         <Card.Body className="pb-5">
           <Card.Img src={image ? image : DefaultProfilePicture} alt="Default Image" className="user-avatar large-avatar rounded-circle mx-auto mt-n7 mb-4" />
           <Card.Title>{name}</Card.Title>
-          <Card.Subtitle>{balance.toFixed(2)}</Card.Subtitle>
+          <Card.Subtitle>{balance}</Card.Subtitle>
         </Card.Body>
       </Card>
     );
