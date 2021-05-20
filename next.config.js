ea3093ca073503f14dbeedf179b0c379f024639d
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-05-20 12:52:45 -0400

    Implement next-secure-headers headers method in next js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withImages = require("next-images");
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = withImages({
     poweredByHeader: false,
     future: {
         webpack5: true,
-    }
+    },
+    async headers() {
+        return [{ source: "/(.*)", headers: createSecureHeaders() }];
+    },
 });
