009c0783aedb48d018037c0c5db10eb0836468bd
Andrew Caruso <10090913+Yzma@users.noreply.github.com>
2021-07-02 00:30:33 -0400

    Change text-xl-left to text-left and text-right to fix spacing on footer

--- components/main/Footer.js
+++ components/main/Footer.js
@@ -8,10 +8,10 @@ export const Footer = () => {
       <footer className="footer section py-4">
         <Row>
           <Col>
-            <p className="text-xl-left ms-4">Copyright © 2021. Andrew Caruso</p>
+            <p className="text-left ms-4">Copyright © 2021. Andrew Caruso</p>
           </Col>
           <Col >
-            <ul className="list-inline list-group-flush list-group-borderless text-xl-right me-4">
+            <ul className="list-inline list-group-flush list-group-borderless text-right me-4">
               <li className="list-inline-item px-0 px-sm-2">
                 <a href="https://github.com/Yzma/Image-Store" target="_blank">Github</a>
               </li>
